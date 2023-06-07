f5a10b23274b5d1081436f0d9407579ba647c498
Shajoshed20 <ugobelle@gmail.com>
2023-06-06 17:58:46 -0700

    formate changed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,6 +8,6 @@ else:
 if lastnum > 5:
     print(f"Last digit of {number} is {lastnum} and is greater than 5")
 elif lastnum == 0:
-    print(f"Last digit of {number} is 0")
+    print(f"Last digit of {number} is 0 and is 0")
 else:
     print(f"Last digit of {number} is {lastnum} and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for ch in range(97, 123):
     if chr(ch) != 'q' and chr(ch) != 'e':
-        print(f"{ch:c}", end="")
+        print("{}".format(chr(ch)), end="")
