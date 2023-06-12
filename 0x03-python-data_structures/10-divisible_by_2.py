dc81d697fca6a75cbf36d47ce54df259e739a6a7
Shajoshed20 <ugobelle@gmail.com>
2023-06-12 22:10:10 +0100

    tasks 8- 12

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    """Find the maximum integer in list"""
+    if len(my_list) == 0:
+        return None
+
+    big_int = my_list[0]
+    for i in my_list:
+        if i > big_int:
+            big_int = i
+    return (big_int)
