b781cc379b6e29bd1a6f05b5203880b361a62318
Shajoshed20 <ugobelle@gmail.com>
2023-06-12 11:49:09 +0100

    chnages made to elif condition

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,7 +4,7 @@ def replace_in_list(my_list, idx, element):
     """replace element at specific location"""
     if idx < 0:
         return (my_list)
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return (my_list)
     else:
         my_list[idx] = element
