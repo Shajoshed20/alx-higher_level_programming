dd44a97cadd521e2707018482315d9a6c5b2e38f
Shajoshed20 <ugobelle@gmail.com>
2023-06-12 22:28:37 +0100

    changed condition statement

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -6,6 +6,7 @@ def divisible_by_2(my_list=[]):
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
             new_list.append(True)
-        new_list.append(False)
+        else:
+            new_list.append(False)
 
     return (new_list)
