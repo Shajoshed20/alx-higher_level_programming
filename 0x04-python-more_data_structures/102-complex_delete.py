84da0eee2341b46b342819ee2a5acf1574ce79a0
Shajoshed20 <ugobelle@gmail.com>
2023-06-16 15:43:39 +0100

    changed modes for files

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    """Delete keys with a specific value in a dictionary."""
+    while value in a_dictionary.values():
+        for key, v in a_dictionary.items():
+            if v == value:
+                del a_dictionary[key]
+                break
+
+    return (a_dictionary)
