cc36840548c6abc6fb589bf684e1bd557f338d09
Shajoshed20 <ugobelle@gmail.com>
2023-06-28 17:34:31 +0100

    Making squares

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a Square class."""
+
+
+class Square:
+    """To show a square."""
+
+    def __init__(self, size):
+        """Initialize a Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
