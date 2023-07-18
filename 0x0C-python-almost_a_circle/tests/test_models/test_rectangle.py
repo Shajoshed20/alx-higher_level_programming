9a64b2146b9007fd42800bfd40e5c011a7dd637b
Shajoshed20 <ugobelle@gmail.com>
2023-07-18 12:50:09 +0100

    edited

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
 
-"""Defines unittests for models/rectangle.py.
+"""Defines unittests for models/rectangle.py."""
 
-Unittest classes:
-    TestRectangle_instantiation - line 25
-    TestRectangle_width - line 114
-    TestRectangle_height - line 190
-    TestRectangle_x - line 262
-    TestRectangle_y - line 334
-    TestRectangle_order_of_initialization - line 402
-    TestRectangle_area - line 430
-    TestRectangle_update_args - line 538
-    TestRectangle_update_kwargs - line 676
-    TestRectangle_to_dictionary - line 788
-"""
+import inspect
 import io
-import sys
 import unittest
+import os
 from models.base import Base
-from models.rectangle import Rectangle
+from models.square import Rectangle
+import sys
 
 
 class TestRectangle_instantiation(unittest.TestCase):
