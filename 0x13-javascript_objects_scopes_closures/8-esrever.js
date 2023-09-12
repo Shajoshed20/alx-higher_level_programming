286e2b77d88c88cdd439d054ee2f6ee2197309a6
Shajoshed20 <ugobelle@gmail.com>
2023-09-13 00:51:52 +0100

    semistand

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-    const current = [];
-    for (let i = list.length - 1; i >= 0; i--) {
-      current.push(list[i]);
-    }
-  
-    return (current);
-  };
+  const current = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    current.push(list[i]);
+  }
+
+  return (current);
+};
