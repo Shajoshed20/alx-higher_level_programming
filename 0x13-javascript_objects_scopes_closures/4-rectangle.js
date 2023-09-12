e30fc59ff74cc752e2e324310848aa601c8a6022
Shajoshed20 <ugobelle@gmail.com>
2023-09-12 23:46:44 +0100

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+// A Rectangle class that defines a rectangle (update of task 3)
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let col = '';
+      for (let j = 0; j < this.width; j++) {
+        col += 'X';
+      }
+      console.log(col);
+    }
+  }
+  rotate () {
+      this.width = this.height;
+      this.height = this.width;
+
+  }
+  double (){
+      this.width = this.width * 2;
+      this.height = this.height * 2;
+  }
+};
