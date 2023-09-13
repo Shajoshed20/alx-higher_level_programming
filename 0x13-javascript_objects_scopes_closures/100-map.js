b118e2a0ca0691148f183ce3f99627135d12985f
Shajoshed20 <ugobelle@gmail.com>
2023-09-13 17:25:32 +0100

    Advanced tasks

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,5 +3,5 @@ const entry = require('./100-data').list;
 
 console.log(entry);
 
-let new_list = entry.map((x, index) => x * index);
-console.log(new_list);
+const newList = entry.map((x, index) => x * index);
+console.log(newList);
